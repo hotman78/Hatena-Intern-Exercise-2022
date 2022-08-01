1699129d00f4a72b5171b90916d7e38b874d25f7
hotman78 <hotmanww@gmail.com>
2022-08-01 11:59:33 +0900

    solve go-exercise-2

--- golang/exercise-2/exercise-slices.go
+++ golang/exercise-2/exercise-slices.go
@@ -3,6 +3,16 @@ package main
 import "golang.org/x/tour/pic"
 
 func Pic(dx, dy int) [][]uint8 {
+	res := make([][]uint8, dx)
+	for x := 0; x < dx; x++ {
+		res[x] = make([]uint8, dy)
+	}
+	for x := 0; x < dx; x++ {
+		for y := 0; y < dy; y++ {
+			res[x][y] = uint8(x%(y+1))
+		}
+	}
+	return res
 }
 
 func main() {
