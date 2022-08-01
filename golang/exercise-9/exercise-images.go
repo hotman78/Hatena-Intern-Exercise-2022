5b5226ed08d8f6b73a6213c80f2f45ace194eb00
hotman78 <hotmanww@gmail.com>
2022-08-01 13:37:03 +0900

    solve go-exercise-9

--- golang/exercise-9/exercise-images.go
+++ golang/exercise-9/exercise-images.go
@@ -1,8 +1,24 @@
 package main
 
-import "golang.org/x/tour/pic"
+import (
+	"golang.org/x/tour/pic"
+	"image/color"
+	"image"
+)
 
 type Image struct{}
+
+func (_ Image)ColorModel()color.Model{
+	return color.RGBAModel
+}
+
+func (_ Image)Bounds()image.Rectangle{
+	return image.Rect(0,0,255,255)
+}
+
+func (img Image)At(x, y int) color.Color{
+	return color.RGBA{uint8(x),uint8(y),255,255}
+}
 
 func main() {
 	m := Image{}
