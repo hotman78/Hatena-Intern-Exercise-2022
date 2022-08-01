eee1838ed02af077d04eddcb47848f29b87646c4
hotman78 <hotmanww@gmail.com>
2022-08-01 12:14:03 +0900

    solve go-exercise-3

--- golang/exercise-3/exercise-maps.go
+++ golang/exercise-3/exercise-maps.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"golang.org/x/tour/wc"
+	"strings"
 )
 
 func WordCount(s string) map[string]int {
-	return map[string]int{"x": 1}
+	var res=make(map[string]int)
+	for _,str:= range strings.Fields(s){
+		if _,ok:=res[str];!ok{
+			res[str]=0
+		}
+		res[str]++
+	}
+	return res
 }
 
 func main() {
