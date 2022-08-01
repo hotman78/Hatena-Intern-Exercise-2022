6dd1a7dae75bf2194eac5a18c3d1f9ca097e4372
hotman78 <hotmanww@gmail.com>
2022-08-01 12:21:13 +0900

    solve go-exercise-4

--- golang/exercise-4/exercise-fibonacci-closure.go
+++ golang/exercise-4/exercise-fibonacci-closure.go
@@ -5,6 +5,13 @@ import "fmt"
 // fibonacci is a function that returns
 // a function that returns an int.
 func fibonacci() func() int {
+	x:=0
+	y:=1
+	return func() int{
+		ret:=x
+		x,y=y,x+y
+		return ret
+	}
 }
 
 func main() {
