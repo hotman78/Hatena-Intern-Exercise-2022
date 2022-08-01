5d8b120d825d4c220a72e84be513c86deed60573
hotman78 <hotmanww@gmail.com>
2022-08-01 13:19:54 +0900

    solve go-exercise-8

--- golang/exercise-8/exercise-rot-reader.go
+++ golang/exercise-8/exercise-rot-reader.go
@@ -10,6 +10,21 @@ type rot13Reader struct {
 	r io.Reader
 }
 
+func (reader rot13Reader)Read(p []byte)(int,error){
+	n,err:=reader.r.Read(p)
+	if err!=nil {
+		return n,err
+	}
+	for i:=0;i<n;i++{
+		if 'a' <= p[i] && p[i] <= 'z'{
+			p[i]=(p[i]-'a'+13)%26+'a'
+		}else if 'A' <= p[i] && p[i] <= 'Z'{
+			p[i]=(p[i]-'A'+13)%26+'A'
+		}
+	}
+	return n,nil
+}
+
 func main() {
 	s := strings.NewReader("Lbh penpxrq gur pbqr!")
 	r := rot13Reader{s}
