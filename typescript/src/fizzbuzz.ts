39eca39a032bdda06615d2ef844c444ba9a59415
hotman78 <hotmanww@gmail.com>
2022-08-02 17:45:28 +0900

    make fizzbuzz with revealing module pattern

--- typescript/src/fizzbuzz.ts
+++ typescript/src/fizzbuzz.ts
@@ -1,8 +1,23 @@
+const CreateFizzBuzzFunc=()=>{
+  let count=0
+  return ()=>{
+    count++
+    if(count%15==0)return "FizzBuzz";
+    if(count%3==0)return "Fizz";
+    if(count%5==0)return "Buzz";
+    return String(count);
+  }
+}
+
 export function initializeFizzBuzz(
   list: HTMLOListElement,
   button: HTMLButtonElement
 ): void {
   console.debug("initializeFizzBuzz", list, button);
-
-  // Implement Me!
+  const FizzBuzzFunc=CreateFizzBuzzFunc();
+  button.onclick=()=>{
+    const liLast = document.createElement('li');
+    liLast.textContent = FizzBuzzFunc();
+    list.appendChild(liLast);
+  }
 }
